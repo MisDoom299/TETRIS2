57255adbd92e832d376dbf7c8445edf1659fe5f1
MisDoom299 <d.shilkin2014@gmail.com>
2024-10-31 17:47:39 +0300

    Initial commit

---
+++ Assets/Sklripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int speed = 20;
+    public bool gameOver = true;
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+
+    if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+
+
+
+
+}
